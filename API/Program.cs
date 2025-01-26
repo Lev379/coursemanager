090f0ffe7412f1a547f7074660ba05eeaa1248e0
Lev_973 <swappa379@gmail.com>
2025-01-26 21:11:04 +0100

    change api cors for localhost

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder =>
-    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:7189", "https://localhost:7189"));
+    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 
