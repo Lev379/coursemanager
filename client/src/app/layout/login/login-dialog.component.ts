90e2a03777a11cc90fefa42ffbd79b5803ae4326
Lev_973 <swappa379@gmail.com>
2025-01-29 21:52:33 +0100

    :sparkles: login dialogue validation and submit

--- client/src/app/layout/login/login-dialog.component.ts
+++ client/src/app/layout/login/login-dialog.component.ts
@@ -1,19 +1,40 @@
-import { Component, ElementRef, model, viewChild, } from '@angular/core';
+import { Component, ElementRef, model, viewChild } from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { Button } from 'primeng/button';
 import { ClickOutsideDirective } from '../../shared/ui/directives/click-outside/click-outside.directive';
+import { InputText } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-dialog',
-  imports: [Dialog, Button, ClickOutsideDirective],
+  imports: [Dialog, Button, ClickOutsideDirective, InputText, FormsModule],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginDialogComponent {
-  backdropEl = viewChild<ElementRef>('backdrop');
   isVisible$ = model(false);
+
+  model = new LoginDto('', '');
 
   closeDialog() {
     this.isVisible$.set(false);
+  }
+
+  onSubmit() {
+    const { username, password } = this.model;
+    console.log(username, password);
+    this.model.clearInput();
+  }
+}
+
+export class LoginDto {
+  constructor(
+    public username: string,
+    public password: string,
+  ) {}
+
+  clearInput() {
+    this.username = '';
+    this.password = '';
   }
 }
