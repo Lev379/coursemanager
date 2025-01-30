28ebc8165df50d8777ac218e85bb3b3609d7439e
Lev_973 <swappa379@gmail.com>
2025-01-30 20:14:57 +0100

    :passport_control: use auth service in login dialog

--- client/src/app/layout/login/login-dialog.component.ts
+++ client/src/app/layout/login/login-dialog.component.ts
@@ -1,40 +1,44 @@
-import { Component, ElementRef, model, viewChild } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { Button } from 'primeng/button';
 import { ClickOutsideDirective } from '../../shared/ui/directives/click-outside/click-outside.directive';
 import { InputText } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../auth/services/auth.service';
+import { LoginModel } from '../../auth/models/Login.model';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-dialog',
-  imports: [Dialog, Button, ClickOutsideDirective, InputText, FormsModule],
+  imports: [
+    Dialog,
+    Button,
+    ClickOutsideDirective,
+    InputText,
+    FormsModule,
+  ],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginDialogComponent {
+  private readonly _authService = inject(AuthService);
+
   isVisible$ = model(false);
 
-  model = new LoginDto('', '');
+  model = new LoginModel('', '');
 
   closeDialog() {
     this.isVisible$.set(false);
   }
 
   onSubmit() {
-    const { username, password } = this.model;
-    console.log(username, password);
-    this.model.clearInput();
-  }
-}
-
-export class LoginDto {
-  constructor(
-    public username: string,
-    public password: string,
-  ) {}
-
-  clearInput() {
-    this.username = '';
-    this.password = '';
+    this._authService.login(this.model).subscribe({
+      next: res => {
+        console.log(res);
+        this.model.clearInput();
+        this.closeDialog();
+      },
+      error: () => console.log('error'),
+    });
   }
 }
