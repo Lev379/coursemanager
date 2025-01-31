822eca7303ae0c8277f4c50c440b3ca85d3f1525
Lev_973 <swappa379@gmail.com>
2025-01-31 21:42:44 +0100

    persist logged-in user in localstorage

--- client/src/app/auth/services/auth.service.ts
+++ client/src/app/auth/services/auth.service.ts
@@ -1,19 +1,36 @@
-import { inject, Injectable, model } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { LoginModel } from '../models/Login.model';
+import { LoginModel } from '../models/login.model';
+import { User } from '../models/user.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly _http = inject(HttpClient);
+
   baseUrl = 'https://localhost:7189/api/';
+
+  currentUser = signal<User | null>(null);
 
   login(model: LoginModel) {
     const { username, password } = model;
-    return this._http.post(this.baseUrl + 'account/login', {
-      username,
-      password,
-    });
+    return this._http
+      .post<User>(this.baseUrl + 'account/login', {
+        username,
+        password,
+      })
+      .pipe(map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
